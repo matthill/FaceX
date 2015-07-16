137a32cfa38ee15b04fe2a63669c119bf619ba7d
Matt Hill <matthew.hill@riverbed.com>
2015-07-16 00:06:23 -0400

    Added wrapper class for facex

---
+++ FaceX/facewrapper.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   facewrapper.cpp
+ * Author: mhill
+ * 
+ * Created on July 15, 2015, 11:55 PM
+ */
+
+#include "facewrapper.h"
+
+facewrapper::facewrapper() : facex("model.xml.gz") {
+  
+}
+
+
+
---
+++ FaceX/facewrapper.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   facewrapper.h
+ * Author: mhill
+ *
+ * Created on July 15, 2015, 11:55 PM
+ */
+
+#ifndef FACEWRAPPER_H
+#define	FACEWRAPPER_H
+
+#include "face_x.h"
+
+class facewrapper {
+public:
+  facewrapper();
+  
+  
+private:
+  FaceX facex;
+
+};
+
+#endif	/* FACEWRAPPER_H */
+
